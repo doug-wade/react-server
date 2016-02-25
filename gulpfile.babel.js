f227cfb423b7abcd75c033af4f9979febcc2e751
Alex Hanson <alex.hanson@redfin.com>
2016-02-25 10:19:38 -0800

    COM-1180 Migrate has.js to a npm module.

---
+++ gulpfile.babel.js
@@ -0,0 +1,62 @@
+import gulp from "gulp";
+import del from "del";
+import eslint from "gulp-eslint";
+import babel from "gulp-babel";
+import RemoveStylesheetImports from "@redfin/babel-plugin-remove-stylesheet-imports";
+import RewriteStingrayImports from "@redfin/babel-plugin-rewrite-stingray-imports";
+
+const JS_FILES = ["./src/**/*.js"];
+
+gulp.task("default", ["build"])
+
+gulp.task("prepublish", ["clean", "build"]);
+
+gulp.task("build", ["lint", "compile-js", "compile-stingray-js"])
+
+gulp.task("lint", function() {
+	return gulp.src(JS_FILES)
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError());
+});
+
+gulp.task("compile-js", () => {
+	return gulp.src(JS_FILES)
+		.pipe(babel())
+		.pipe(gulp.dest("./"));
+});
+
+gulp.task("compile-stingray-js", () => {
+	return gulp.src(JS_FILES)
+		.pipe(babel({
+			plugins: [
+				RemoveStylesheetImports,
+				RewriteStingrayImports,
+				"transform-es2015-modules-amd",
+			],
+		}))
+		.pipe(babel({
+			// The transform-es2015-modules-amd plugin has a regression as
+			// of November 2015 or so where reserved words like "default"
+			// are not being quoted properly. This upsets Dojo's rollup
+			// builder. Adding these plugins to the list above doesn't
+			// work, but doing them in a second Babel pass does.
+			//     https://phabricator.babeljs.io/T2817
+			//     https://phabricator.babeljs.io/T6863
+			plugins: [
+				"transform-es3-member-expression-literals",
+				"transform-es3-property-literals",
+			],
+
+			// Avoid redundant inherited steps.
+			babelrc: false,
+		}))
+		.pipe(gulp.dest("./stingray/"));
+});
+
+gulp.task("clean", () => {
+	return del([
+		"./has.js",
+		"./stingray",
+	])
+});
